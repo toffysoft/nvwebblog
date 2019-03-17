bcf3ccf69a59e5c7e58c88c7b95fb8198c61216d
natgooddev <gooddev639@gmail.com>
2019-03-17 19:11:41 +0700

    less 11 - blog front end

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -6,7 +6,7 @@
         <li><router-link :to="{name: 'users'}" >Users</router-link></li>
         <li><router-link :to="{name: 'comments'}" >Comments</router-link></li>
         <li><router-link :to="{name: 'login'}" >Login</router-link></li>
-        <li><router-link :to="{name: 'blogs'}" >Blogs</router-link></li>
+        <li><a href="#">Logout</a></li>
       </ul>
       <div class="clearfix"></div>
     </div>   
