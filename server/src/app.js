15ec516734511ae1e39b43e59d72d27ceba8aabf
natgooddev <gooddev639@gmail.com>
2019-03-17 14:04:29 +0700

    less 4 - easy post RESTFul API

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,10 @@
 let express = require('express')
+let bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/status', function (req, res ){
   res.send('Hello nodejs server')
@@ -13,12 +17,29 @@ app.get('/hello/:person', function (req,res) {
 
 // get user by id
 app.get('/user/:userId', function (req, res) {
-  res.send('ดูข้อมูลผู้ใช้งาน')
+  res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId)
 })
 
 // get all user
 app.get('/users', function (req, res) {
   res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
+})
+
+// create user
+app.post('/user/', function (req, res) {
+  res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
+})
+
+// edit user
+app.put('/user/:userId', function (req, res) {
+  res.send('ทำการแก้ไขผุ้ใช้งาน: ' + req.params.userId + ' : ' +
+  JSON.stringify(req.body))
+})
+
+// delete user
+app.delete('/user/:userId', function (req, res) {
+  res.send('ทำการลบผุ้ใช้งาน: ' + req.params.userId + ' : ' + 
+  JSON.stringify(req.body)) 
 })
 
 let port = 8081
