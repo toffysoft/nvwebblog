946e3737a544dde4a742fb129f8591d2521aaacd
natgooddev <gooddev639@gmail.com>
2019-03-17 18:48:55 +0700

    less 11 - blog system on back end

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,36 @@
 const UserController = require('./controllers/UserController')
 const UserAuthenController = require('./controllers/UserAuthenController')
 const isAuthenController = require('./authen/isAuthenController')
+const BlogController = require('./controllers/BlogController')
+
 
 module.exports = (app) => {
+  // blog route
+  // create blog
+  app.post('/blog',
+  BlogController.create
+  )
+
+  // edit blog, suspend, active
+  app.put('/blog/:blogId',
+  BlogController.put
+  )
+
+  // delete blog
+  app.delete('/blog/:blogId',
+  BlogController.remove
+  )
+
+  // get blog by id
+  app.get('/blog/:blogId',
+  BlogController.show
+  )
+
+  // get all blog
+  app.get('/blogs',
+  BlogController.index   
+  )
+
   // authen
   app.post('/login', 
       UserAuthenController.login
