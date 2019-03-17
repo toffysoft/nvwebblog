07c763b37408af1abcf4b3caf957ea48044c9220
natgooddev <gooddev639@gmail.com>
2019-03-17 15:13:22 +0700

    less 8 - load and display data on vuejs

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
     <h1>Get All Users</h1>
+    <div v-if="users.length">
+      <div>จำนวนผู้ใช้งาน {{ users.length }}</div>
+      <div>id: {{ users[0].id }}</div>
+      <div>ชื่อ-นามสกุล: {{ users[0].name }} - {{ users[0].lastname }}</div>
+      <div>email: {{ users[0].email }}</div>
+      <div>password: {{ users[0].password }}</div>
+    </div>
   </div>
 </template>
 <script>
 import UsersService from '@/services/UsersService'
 
 export default {
+  data () {
+    return {
+      users: []
+    }
+  },
   async created () {
-    let results = (await UsersService.index()).data
-    console.log(results)
+    this.users = (await UsersService.index()).data
   }
 }
 </script>
