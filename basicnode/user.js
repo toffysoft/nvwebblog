11a756d9125d608bac0a2e908fbe5d9c1f290e4a
natgooddev <gooddev639@gmail.com>
2019-03-17 13:36:22 +0700

    less 2 - access to json data

--- basicnode/user.js
+++ basicnode/user.js
@@ -5,4 +5,9 @@ let user = {
   "from":"Thailand"
 }
 
-console.log(user)
+console.log('Name: ' + user.name + ' ' + user.lastname)
+
+user.name = "Steve"
+user.lastname = "Job"
+
+console.log('New Data - Name: ' + user.name + ' ' + user.lastname)
