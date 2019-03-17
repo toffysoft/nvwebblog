59e2e7dcbe7c0cb27c01e9cdf30afc04facde2fd
natgooddev <gooddev639@gmail.com>
2019-03-17 13:43:52 +0700

    less 2 - json array and manage

--- basicnode/user.js
+++ basicnode/user.js
@@ -1,13 +1,143 @@
-let user = {
-  "name":"chayapon",
-  "lastname":"chandra",
-  "email":"gooddev639@gmail.com",
-  "from":"Thailand"
+let users = [
+  {
+    "name":"chayapon",
+    "lastname":"chandra",
+    "email":"gooddev639@gmail.com",
+    "from":"Thailand"
+  },
+  {
+    "name":"Job",
+    "lastname":"Steve",
+    "email":"steve.job@gmail.com",
+    "from":"USA"
+  },
+]
+
+// # sample 1 ----------------
+/* console.log('Name: ' + users[0].name + ' ' + users[0].lastname)
+console.log('Name: ' + users[1].name + ' ' + users[1].lastname)
+
+for (let i = 0; i < 2; i++) {
+  console.log('Name: ' + users[i].name + ' ' + users[i].lastname)
+} */
+
+
+// # sample 2 ------------------
+/* let userA = 
+  {
+    "name":"AAA",
+    "lastname":"AAAAA",
+    "email":"AAA@gmail.com",
+    "from":"Thailand"
+  }
+
+  let userB = 
+  {
+    "name":"BBB",
+    "lastname":"BBBBB",
+    "email":"BBB@gmail.com",
+    "from":"Thailand"
+  }
+
+users.push(userA)
+users.push(userB)
+
+for (let i = 0; i < users.length; i++) {
+  console.log('Name: ' + users[i].name + ' ' + users[i].lastname)
+} */
+
+// #sample 3 --------------------
+/* let userA = 
+  {
+    "name":"AAA",
+    "lastname":"AAAAA",
+    "email":"AAA@gmail.com",
+    "from":"Thailand"
+  }
+
+  let userB = 
+  {
+    "name":"BBB",
+    "lastname":"BBBBB",
+    "email":"BBB@gmail.com",
+    "from":"Thailand"
+  }
+
+users.push(userA)
+users.push(userB)
+
+let userSelect = users.splice(2,2)
+
+for (let i = 0; i < users.length; i++) {
+  console.log('Name: ' + users[i].name + ' ' + users[i].lastname)
 }
 
-console.log('Name: ' + user.name + ' ' + user.lastname)
+console.log('----------------------')
 
-user.name = "Steve"
-user.lastname = "Job"
+for (let i = 0; i < userSelect.length; i++) {
+  console.log('Name: ' + userSelect[i].name + ' ' + userSelect[i].lastname)
+} */
 
-console.log('New Data - Name: ' + user.name + ' ' + user.lastname)
+// #sample 4 ---------------------
+/* let userA =
+  {
+  "name":"AAA",
+  "lastname":"AAAAA",
+  "email":"AAA@gmail.com",
+  "from":"Thailand"
+  }
+
+let userB =
+  {
+  "name":"BBB",
+  "lastname":"BBBBB",
+  "email":"BBB@gmail.com",
+  "from":"Thailand"
+  }
+
+users.push(userA)
+users.push(userB)
+
+// delete users.splice(0,1)
+
+for (let i = 0; i < users.length; i++) {
+  console.log('Name: ' + users[i].name + ' ' + users[i].lastname)
+}
+
+let userSelect = users.splice(2,2)
+console.log('------------------')
+
+for (let i = 0; i < userSelect.length; i++) {
+  console.log('Name: ' + userSelect[i].name + ' ' + userSelect[i].lastname)
+} */
+
+let userA =
+{
+"name":"AAA",
+"lastname":"AAAAA",
+"email":"AAA@gmail.com",
+"from":"Thailand"
+}
+
+let userB =
+{
+"name":"BBB",
+"lastname":"BBBBB",
+"email":"BBB@gmail.com",
+"from":"Thailand"
+}
+
+users.push(userA)
+users.push(userB)
+
+for (let i = 0; i < users.length; i++) {
+console.log('Name: ' + users[i].name + ' ' + users[i].lastname)
+}
+
+delete users.splice(0,1)
+
+console.log('\n*** After Delete\n')
+
+for (let i = 0; i < users.length; i++) {
+console.log('Name: ' + users[i].name + ' ' + users[i].lastname)
+}
