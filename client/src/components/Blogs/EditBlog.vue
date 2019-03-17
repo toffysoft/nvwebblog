9064717ff11281230a96b1cc7615144c5b012bb6
natgooddev <gooddev639@gmail.com>
2019-03-17 21:36:37 +0700

    less 12 - CKEditor on blog edit

--- client/src/components/Blogs/EditBlog.vue
+++ client/src/components/Blogs/EditBlog.vue
@@ -3,7 +3,8 @@
     <h1>Edit Blog</h1>
     <form v-on:submit.prevent = "editBlog">
       <p>title: <input type="text" v-model="blog.title"></p>
-      <p>content: <input type="text" v-model="blog.content"></p>
+      <p><strong>content: </strong></p>
+      <p><vue-ckeditor v-model.lazy="blog.content" :config="config" @blur="onBlur($event)" @focus="onFocus($event)" /></p>
       <p>category: <input type="text" v-model="blog.category"></p>
       <p>status: <input type="text" v-model="blog.status"></p>
       <p>
@@ -15,7 +16,7 @@
  </template>
  <script>
  import BlogsService from '@/services/BlogsService'
- 
+ import VueCkeditor from "vue-ckeditor2"
  export default {
   data () {
     return {
@@ -26,8 +27,17 @@
         content: '',
         category: '',
         status: ''
+      },
+      config: {
+        toolbar: [
+          ["Bold", "Italic", "Underline", "Strike", "Subscript", "Superscript"]
+        ],
+        height: 300
       }
     }
+  },
+  components: { 
+    VueCkeditor 
   },
   methods: {
     async editBlog () {
@@ -39,7 +49,13 @@
       } catch (err) {
         console.log(err)
       }
-    }
+    },
+    onBlur (editor) {
+      console.log(editor);
+    },
+    onFocus (editor) {
+      console.log(editor);
+    },
   },
   async created () {
     try {
@@ -47,8 +63,112 @@
       this.blog = (await BlogsService.show(blogId)).data
     } catch (error) {
       console.log (error)
-    }  
-  }
+    }
+    
+    this.config.toolbar = [
+      {
+        name: "document",
+        items: [
+          "Source",
+          "-",
+          "Save",
+          "NewPage",
+          "Preview",
+          "Print",
+          "-",
+          "Templates"
+        ]
+      },
+      {
+        name: "clipboard",
+        items: [
+          "Cut",
+          "Copy",
+          "Paste",
+          "PasteText",
+          "PasteFromWord",
+          "-",
+          "Undo",
+          "Redo"
+        ]
+      },
+      {
+        name: "editing",
+        items: ["Find", "Replace", "-", "SelectAll", "-", "Scayt"]
+      },
+      {
+        name: "forms",
+        items: [
+          "Form",
+          "Checkbox",
+          "Radio",
+          "TextField",
+          "Textarea",
+          "Select",
+          "Button",
+          "ImageButton",
+          "HiddenField"
+        ]
+      },
+      "/",
+      {
+        name: "basicstyles",
+        items: [
+          "Bold",
+          "Italic",
+          "Underline",
+          "Strike",
+          "Subscript",
+          "Superscript",
+          "-",
+          "CopyFormatting",
+          "RemoveFormat"
+        ]
+      },
+      {
+        name: "paragraph",
+        items: [
+          "NumberedList",
+          "BulletedList",
+          "-",
+          "Outdent",
+          "Indent",
+          "-",
+          "Blockquote",
+          "CreateDiv",
+          "-",
+          "JustifyLeft",
+          "JustifyCenter",
+          "JustifyRight",
+          "JustifyBlock",
+          "-",
+          "BidiLtr",
+          "BidiRtl",
+          "Language"
+        ]
+      },
+      { name: "links", items: ["Link", "Unlink", "Anchor"] },
+      {
+        name: "insert",
+        items: [
+          "Image",
+          "Flash",
+          "Table",
+          "HorizontalRule",
+          "Smiley",
+          "SpecialChar",
+          "PageBreak",
+          "Iframe",
+          "InsertPre"
+        ]
+      },
+      "/",
+      { name: "styles", items: ["Styles", "Format", "Font", "FontSize"] },
+      { name: "colors", items: ["TextColor", "BGColor"] },
+      { name: "tools", items: ["Maximize", "ShowBlocks"] },
+      { name: "about", items: ["About"] }
+    ]
+  },
 }
 </script>
 <style scoped>
