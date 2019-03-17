ef18ac57ff062b6b463a1ece0fffb7366a6b681a
natgooddev <gooddev639@gmail.com>
2019-03-17 14:00:11 +0700

    easy get RESTFul API

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,27 @@
 let express = require('express')
+
 const app = express()
 
-app.get('/status', function (req, res) {
-  res.send('Hello node js server')
+app.get('/status', function (req, res ){
+  res.send('Hello nodejs server')
+})
+
+app.get('/hello/:person', function (req,res) {
+  console.log('hello - ' + req.params.person)
+  res.send('sey hello with ' + req.params.person)
+})
+
+// get user by id
+app.get('/user/:userId', function (req, res) {
+  res.send('ดูข้อมูลผู้ใช้งาน')
+})
+
+// get all user
+app.get('/users', function (req, res) {
+  res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
 })
 
 let port = 8081
-
 app.listen(port, function () {
-  console.log(`server running on ${port}`)
+  console.log('server running on ' + port)
 })
