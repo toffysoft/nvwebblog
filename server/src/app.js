09348d294fc61e55fdee6e6e829d42e96d72ba75
natgooddev <gooddev639@gmail.com>
2019-03-17 13:56:21 +0700

    less 4 - getting start with express

---
+++ server/src/app.js
@@ -0,0 +1,12 @@
+let express = require('express')
+const app = express()
+
+app.get('/status', function (req, res) {
+  res.send('Hello node js server')
+})
+
+let port = 8080
+
+app.listen(port, function () {
+  console.log(`server running on ${port}`)
+})
