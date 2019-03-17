171b049a5cfd8f0961b81150c650d93ef2464afe
natgooddev <gooddev639@gmail.com>
2019-03-17 22:20:22 +0700

    less 13 - upload and display uploaded pictrue

--- client/src/components/Blogs/CreateBlog.vue
+++ client/src/components/Blogs/CreateBlog.vue
@@ -3,6 +3,30 @@
     <h1>Create Blog</h1>
     <form v-on:submit.prevent = "createBlog">
       <p>title: <input type="text" v-model="blog.title"></p>
+      <form enctype="multipart/form-data" novalidate>
+        <div class="dropbox">
+          <input type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length"
+            accept="image/*" class="input-file">
+            <!-- <p v-if="isInitial || isSuccess"> -->
+            <p v-if="isInitial">
+              Drag your file(s) here to begin<br> or click to browse
+            </p>
+            <p v-if="isSaving">
+              Uploading {{ fileCount }} files...
+            </p>   
+            <p v-if="isSuccess">
+              Upload Successful.
+            </p>        
+        </div>
+      </form>
+      <div>
+        <ul class="pictures">
+          <li v-for="picture in pictures" v-bind:key="picture.id">              
+            <img style="margin-bottom:5px;" :src="BASE_URL+picture.name" alt="picture image">              
+          </li>
+        </ul>
+        <div class="clearfix"></div>
+      </div>  
       <p><strong>content: </strong></p>
       <p><vue-ckeditor v-model.lazy="blog.content" :config="config" @blur="onBlur($event)" @focus="onFocus($event)" /></p>
       <p>category: <input type="text" v-model="blog.category"></p>
@@ -14,6 +38,12 @@
 <script>
 import BlogsService from '@/services/BlogsService'
 import VueCkeditor from "vue-ckeditor2"
+import UploadService from '@/services/UploadService'
+
+const STATUS_INITIAL = 0,
+  STATUS_SAVING = 1,
+  STATUS_SUCCESS = 2,
+  STATUS_FAILED = 3
 
 export default {
   data () {
@@ -31,10 +61,82 @@ export default {
           ["Bold", "Italic", "Underline", "Strike", "Subscript", "Superscript"]
         ],
         height: 300
-      }
+      },
+      // upload data
+      BASE_URL: "http://localhost:8081/assets/uploads/",
+      error: null,      
+      uploadError: null,
+      currentStatus: null,
+      uploadFieldName: "userPhoto",      
+      uploadedFileNames:[],
+      pictures: [],
+      pictureIndex: 0,
     }
   },
-  methods: {
+  methods: {// upload
+    wait(ms) {
+      return x => {
+        return new Promise(resolve => setTimeout(() => resolve(x), ms));
+      };
+    },    
+    reset() {
+      // reset form to initial state
+      this.currentStatus = STATUS_INITIAL
+      // this.uploadedFiles = []
+      this.uploadError = null   
+      this.uploadedFileNames = []
+    },
+    async save(formData) {
+      // upload data to the server
+      try {
+        this.currentStatus = STATUS_SAVING
+        await UploadService.upload(formData)
+        this.currentStatus = STATUS_SUCCESS
+
+        // update image uploaded display
+        let pictureJSON = []
+        this.uploadedFileNames.forEach(uploadFilename => {
+          let found = false;
+          for(let i=0;i<this.pictures.length;i++){
+            if(this.pictures[i].name == uploadFilename){            
+              found = true            
+              break
+            }            
+          }
+
+          if(!found){
+            this.pictureIndex++
+            let pictureJSON = {
+              id: this.pictureIndex,
+              name: uploadFilename
+            }
+            this.pictures.push(pictureJSON) 
+          }  
+        })
+
+      } catch (error) {
+        console.log(error)
+        this.currentStatus = STATUS_FAILED
+      }
+    },
+    filesChange(fieldName, fileList) {
+      // handle file changes
+      const formData = new FormData();
+
+      if (!fileList.length) return;
+
+      // append the files to FormData
+      Array.from(Array(fileList.length).keys()).map(x => {
+        formData.append(fieldName, fileList[x], fileList[x].name);
+        this.uploadedFileNames.push(fileList[x].name);
+      });
+
+      // save it
+      this.save(formData);
+    },
+    clearUploadResult: function(){            
+      setTimeout(() => this.reset(), 5000);
+    },
     async createBlog () {
       try {
         await BlogsService.post(this.blog)
@@ -55,7 +157,22 @@ export default {
   components: { 
     VueCkeditor 
   },
+  computed: {
+    isInitial() {
+      return this.currentStatus === STATUS_INITIAL;
+    },
+    isSaving() {
+      return this.currentStatus === STATUS_SAVING;
+    },
+    isSuccess() {
+      return this.currentStatus === STATUS_SUCCESS;
+    },
+    isFailed() {
+      return this.currentStatus === STATUS_FAILED;
+    }
+  },
   created () {
+    this.reset(),
     this.config.toolbar = [
       {
         name: "document",
@@ -163,4 +280,57 @@ export default {
 }
 </script>
 <style scoped>
+/* uplaod */
+.dropbox {
+  outline: 2px dashed grey; /* the dash box */
+  outline-offset: -10px;
+  background:lemonchiffon;
+  color: dimgray;
+  padding: 10px 10px;
+  min-height: 200px; /* minimum height */
+  position: relative;
+  cursor: pointer;
+}
+
+.input-file {
+  opacity: 0; /* invisible but it's there! */
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  cursor: pointer;
+}
+
+.dropbox:hover {
+  background:khaki; /* when mouse over to the drop zone, change color */
+}
+
+.dropbox p {
+  font-size: 1.2em;
+  text-align: center;
+  padding: 50px 0;
+}
+
+/* display uplaod */
+ul.pictures {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  float: left;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+ul.pictures li {
+  float: left;
+}
+
+ul.pictures li img {
+  max-width: 180px;
+  margin-right: 20px;
+}
+
+.clearfix {
+  clear: both;
+}
+
 </style>
