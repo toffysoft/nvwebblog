e11eeeca4b29f1cd1bcdca61b72294af3d5074c7
natgooddev <gooddev639@gmail.com>
2019-03-19 17:14:13 +0700

    less 18 - from sqlite to mysql

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,12 +1,13 @@
 module.exports = {
   port: 8081,
     db: {
-      database: process.env.DB_NAME || 'nvWebblogDb',
+      database: process.env.DB_NAME || 'nvwebblog',
       user: process.env.DB_User || 'root',
       password: process.env.DB_PASS || '',
       options: {
-        dialect: process.env.DIALECT || 'sqlite',
-        storage: './nvwebblog-db.sqlite'    
+        dialect: process.env.DIALECT || 'mysql',
+      //   storage: './nvwebblog-db.sqlite'    
+        host: process.env.HOST || 'localhost',
       },   
     },
     authentication: {
